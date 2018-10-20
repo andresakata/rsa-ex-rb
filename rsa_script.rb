3bca1070ae9ab7f577e7528b9c94c89fb385dc8b
Andre Sakata <andregsakata@gmail.com>
2018-10-20 12:31:46 -0300

    Refactor

--- rsa_script.rb
+++ rsa_script.rb
@@ -6,26 +6,20 @@ require_relative 'lib/key_generator'
 require_relative 'lib/brutal_force_decryptor'
 require_relative 'lib/pollard_rho_decryptor'
 
-public_key, private_key = KeyGenerator.generate_key(128, 256)
+public_key, private_key = KeyGenerator.generate_key(2**7, 2**8)
 
 file = File.read('file')
-p file
+puts file
 
-p '-- Encrypting using public key'
+puts '-- Encrypting using public key'
 c = RsaEncryptor.new(public_key).encrypt(file)
 p c
 
-p '-- Decrypting using private key'
-m = RsaDecryptor.new(private_key).decrypt(c)
-p m
+puts '-- Decrypting using private key'
+puts RsaDecryptor.new(private_key).decrypt(c)
 
-p '-- Brutal force'
-brutal_force_decryptor = BrutalForceDecryptor.new(public_key)
-forced_private_key = brutal_force_decryptor.decrypt
-forced_m = RsaDecryptor.new(forced_private_key).decrypt(c)
-p forced_m
+puts '-- Brutal force'
+puts RsaDecryptor.new(BrutalForceDecryptor.new(public_key).decrypt).decrypt(c)
 
-p '-- Pollard rho'
-pollard_private_key = PollardRhoDecryptor.new(public_key).decrypt
-pollard_m = RsaDecryptor.new(pollard_private_key).decrypt(c)
-p pollard_m
+puts '-- Pollard rho'
+puts RsaDecryptor.new(PollardRhoDecryptor.new(public_key).decrypt).decrypt(c)
