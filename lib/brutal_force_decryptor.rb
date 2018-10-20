305c0a59f20e282ea3f95221bb63000e2f14f2df
Andre Sakata <andregsakata@gmail.com>
2018-10-20 09:31:13 -0300

    Add brutal force decryptor algorithm

---
+++ lib/brutal_force_decryptor.rb
@@ -0,0 +1,22 @@
+require_relative 'utils'
+
+class BrutalForceDecryptor
+  def self.decrypt(public_key)
+    should_break = false
+    for i in 0..KeyGenerator::PRIME_MAX do
+      next unless i.odd? && Utils.prime?(i)
+      for j in 0..KeyGenerator::PRIME_MAX do
+        next unless i.odd? && Utils.prime?(j)
+        if i * j == public_key[1]
+          should_break = true
+          break
+        end
+      end
+      break if should_break
+    end
+    [
+      KeyGenerator.determine_d(public_key[0], (i - 1) * (j - 1)),
+      public_key[1]
+    ]
+  end
+end
--- rsa_script.rb
+++ rsa_script.rb
@@ -1,9 +1,19 @@
 require_relative 'lib/rsa_encryptor'
 require_relative 'lib/rsa_decryptor'
 require_relative 'lib/key_generator'
+require_relative 'lib/brutal_force_decryptor'
 
 public_key, private_key = KeyGenerator.generate_key
+
+p '-- Encrypting using public key'
 c = RsaEncryptor.new(public_key).encrypt(File.read('file'))
 p c
+
+p '-- Decrypting using private key'
 m = RsaDecryptor.new(private_key).decrypt(c)
 p m
+
+p '-- Brutal force'
+forced_private_key = BrutalForceDecryptor.decrypt(public_key)
+forced_m = RsaDecryptor.new(forced_private_key).decrypt(c)
+p forced_m
