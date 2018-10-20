c349e01b99db27a1fc306962834a2ec09c8b91bf
Andre Sakata <andregsakata@gmail.com>
2018-10-20 12:18:22 -0300

    Remove brutal force method

--- lib/brutal_force_decryptor.rb
+++ lib/brutal_force_decryptor.rb
@@ -7,26 +7,7 @@ class BrutalForceDecryptor
     @public_key = public_key
   end
 
-  def decrypt_1
-    should_break = false
-    for i in 0..KeyGenerator::PRIME_MAX do
-      next unless i.odd? && Utils.prime?(i)
-      for j in 0..KeyGenerator::PRIME_MAX do
-        next unless i.odd? && Utils.prime?(j)
-        if i * j == @public_key[1]
-          should_break = true
-          break
-        end
-      end
-      break if should_break
-    end
-    [
-      KeyGenerator.determine_d(@public_key[0], (i - 1) * (j - 1)),
-      @public_key[1]
-    ]
-  end
-
-  def decrypt_2
+  def decrypt
     p = 0
     for i in 2..Integer.sqrt(@public_key[1])
       if @public_key[1] % i == 0
