6c0fac9e0cb78f99b4ecc7523dc1b12f9cef4087
Andre Sakata <andregsakata@gmail.com>
2018-10-23 20:55:27 -0300

    Improve tests

--- spec/lib/brute_force_decryptor_spec.rb
+++ spec/lib/brute_force_decryptor_spec.rb
@@ -125,6 +125,33 @@ RSpec.describe BruteForceDecryptor do
       end
     end
 
+    context 'when public key has 2**42 key size' do
+      it 'decrypt message' do
+        logger_pollard = create_logger('pollard_rho_42.log')
+        for i in 1..5 do
+          run_decrypt(2**42, 2**43, nil, logger_pollard)
+        end
+      end
+    end
+
+    context 'when public key has 2**44 key size' do
+      it 'decrypt message' do
+        logger_pollard = create_logger('pollard_rho_44.log')
+        for i in 1..5 do
+          run_decrypt(2**44, 2**45, nil, logger_pollard)
+        end
+      end
+    end
+
+    context 'when public key has 2**46 key size' do
+      it 'decrypt message' do
+        logger_pollard = create_logger('pollard_rho_46.log')
+        for i in 1..5 do
+          run_decrypt(2**46, 2**47, nil, logger_pollard)
+        end
+      end
+    end
+
     def run_decrypt(min, max, logger_brute, logger_pollard)
       public_key, private_key = KeyGenerator.generate_key(min, max)
       if 2**32 > min
